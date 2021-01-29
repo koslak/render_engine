4d563cf25fa5ede045f252d0449ef697c5d5ae7d
Luis M. Betancourt <lm.betancourt@me.com>
2021-01-29 06:57:02 -0500

    Changing the sphere hit function when quadratic equation if b=2h.

--- src/cameras/camera.cpp
+++ src/cameras/camera.cpp
@@ -27,16 +27,16 @@ Ray Camera::get_ray(double u, double v) const noexcept
 double hit_sphere(const DFL::Point3d<double>& center, double radius, const Ray& r) noexcept
 {
     DFL::Vector3d<double> oc = r.origin - center;
-    auto a = dot(r.direction, r.direction);
-    auto b = 2.0 * dot(oc, r.direction);
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction.length_squared();
+    auto half_b = dot(oc, r.direction);
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
 
     if(discriminant < 0)
     {
         return -1.0;
     }else{
-        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+        return (-half_b - std::sqrt(discriminant)) / a;
     }
 }
 
