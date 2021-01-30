2e2be665ae92ea9f98aaab04748f1fffa3375e70
Luis M. Betancourt <lm.betancourt@me.com>
2021-01-30 05:43:01 -0500

    Common constants and utility functions added to the DFL.h file.

--- src/core/dfl.h
+++ src/core/dfl.h
@@ -2,10 +2,20 @@
 #define DFL_H
 
 #include <cmath>
+#include <limits>
+#include <memory>
 #include <cassert>
 #include <iostream>
 
 // Use (void) to silent unused warnings.
 #define assertm(exp, msg) assert(((void)msg, exp))
+
+constexpr const double infinity{ std::numeric_limits<double>::infinity() };
+constexpr const double PI{ 3.1415926535897932385 };
+
+inline double degrees_to_radians(double degrees)
+{
+    return degrees * PI / 180.0;
+}
 
 #endif // DFL_H
